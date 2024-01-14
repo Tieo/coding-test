33c46e08d928bfca556ab5b037263977120c24a5
Replit user <>
2024-01-14 14:30:50 +0000

    initial commit

---
+++ chekRunnigTime.py
@@ -0,0 +1,12 @@
+# ctrl+. -> check command
+from random import randint
+import time
+
+start_time = time.time()  # start
+
+# check code
+print('wow')
+time.sleep(6)
+
+end_time = time.time()  # end
+print("test result: ", end_time - start_time)  #print
